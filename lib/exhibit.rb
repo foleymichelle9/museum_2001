3c93252fbff2156cecdba19a3e53ad06214b12b9
michelle <foleymichelle9@gmail.com>
2020-02-19 09:31:00 -0700

    Fix test errors and adds exhibit class wooo

---
+++ lib/exhibit.rb
@@ -0,0 +1,8 @@
+class Exhibit
+
+  attr_reader :name
+  
+  def initialize(attributes)
+    @name = attributes[:name]
+  end
+end
--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,11 +1,13 @@
-require './lib/exhibit'
-require './lib/patron'
 require "minitest/autorun"
 require "minitest/pride"
+require './lib/exhibit'
+require './lib/patron'
+
 
 class ExhibitTest < Minitest::Test
 
   def test_it_exists
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     assert_instance_of Exhibit, exhibit
   end
 
@@ -14,3 +16,32 @@ class ExhibitTest < Minitest::Test
     assert_equal "Gems and Minerals", exhibit.name
   end
 end
+
+
+#pry(main)> exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+# => #<Exhibit:0x00007fcb13bd22d0...>
+
+#pry(main)> exhibit.name
+# => "Gems and Minerals"
+
+#pry(main)> exhibit.cost
+# => 0
+
+#pry(main)> patron_1 = Patron.new("Bob", 20)
+# => #<Patron:0x00007fcb13b5c7d8...>
+
+#pry(main)> patron_1.name
+# => "Bob"
+
+#pry(main)> patron_1.spending_money
+# => 20
+
+#pry(main)> patron_1.interests
+# => []
+
+#pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+
+#pry(main)> patron_1.add_interest("Gems and Minerals")
+
+#pry(main)> patron_1.interests
+# => ["Dead Sea Scrolls", "Gems and Minerals"]
