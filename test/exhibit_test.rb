d74da2d9c5994f694bc1e57395ee996fd622db52
michelle <foleymichelle9@gmail.com>
2020-02-19 09:23:26 -0700

    Moves test to correct test file

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,35 +1,16 @@
-```ruby
-pry(main)> require './lib/exhibit'
-# => true
+require './lib/exhibit'
+require './lib/patron'
+require "minitest/autorun"
+require "minitest/pride"
 
-pry(main)> require './lib/patron'
-# => true
+class ExhibitTest < Minitest::Test
 
-pry(main)> exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-# => #<Exhibit:0x00007fcb13bd22d0...>
+  def test_it_exists
+    assert_instance_of Exhibit, exhibit
+  end
 
-pry(main)> exhibit.name
-# => "Gems and Minerals"
-
-pry(main)> exhibit.cost
-# => 0
-
-pry(main)> patron_1 = Patron.new("Bob", 20)
-# => #<Patron:0x00007fcb13b5c7d8...>
-
-pry(main)> patron_1.name
-# => "Bob"
-
-pry(main)> patron_1.spending_money
-# => 20
-
-pry(main)> patron_1.interests
-# => []
-
-pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-
-pry(main)> patron_1.add_interest("Gems and Minerals")
-
-pry(main)> patron_1.interests
-# => ["Dead Sea Scrolls", "Gems and Minerals"]
-```
+  def test_it_has_a_name
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    assert_equal "Gems and Minerals", exhibit.name
+  end
+end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -1,19 +1,4 @@
-require './lib/exhibit'
-require './lib/patron'
-require "minitest/autorun"
-require "minitest/pride"
 
-class ExhibitTest < Minitest::Test
-
-  def test_it_exists
-    assert_instance_of Exhibit, exhibit
-  end
-
-  def test_it_has_a_name
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    assert_equal "Gems and Minerals", exhibit.name
-  end 
-end
 
 
 
