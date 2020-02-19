da4f9e5a6062a266fc2fcdaad69c8d4cca3c6c22
michelle <foleymichelle9@gmail.com>
2020-02-19 10:53:42 -0700

    Test it can add new interests, write method to add new interests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -17,6 +17,13 @@ class PatronTest < Minitest::Test
     assert_equal 20, patron_1.spending_money
     assert_equal [], patron_1.interests
   end
+
+  def test_it_can_add_new_interests
+    assert_equal [], patron_1.interests
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  end
 end
 
 
@@ -43,8 +50,7 @@ end
 
 
 
-#pry(main)> patron_1.interests
-# => []
+
 
 #pry(main)> patron_1.add_interest("Dead Sea Scrolls")
 
